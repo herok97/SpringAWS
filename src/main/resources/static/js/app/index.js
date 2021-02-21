490a9b73ee85c4d1976dfdf7188e0e1d814aa83a
duddn <herok97>
2021-02-22 08:11:45 +0900

    0222-1

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -37,6 +37,8 @@ var main = {
             content: $('#content').val()
         };
 
+        var id = $('#id').val();
+
         $.ajax({
             type: 'PUT',
             url: '/api/v1/posts/'+id,
@@ -49,6 +51,23 @@ var main = {
         }).fail(function (error){
             alert(JSON.stringify(error));
         });
+    }
+
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+
     }
 };
 
