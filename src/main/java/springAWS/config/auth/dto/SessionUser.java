7bab9917cddc7a70f162a305c8dbe32650852bc0
duddn <herok97>
2021-02-23 08:54:38 +0900

    0223-1

---
+++ src/main/java/springAWS/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package springAWS.config.auth.dto;
+
+import lombok.Getter;
+import springAWS.domain.user.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
